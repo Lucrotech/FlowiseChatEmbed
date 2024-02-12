01d078e7d55a7638a1f4b1f4a3f256c88c8a2f31
Werner Koegelenberg <werner@lucrotech.co.za>
2024-02-12 12:28:45 +0200

    Update Bubble.tsx

--- src/features/bubble/components/Bubble.tsx
+++ src/features/bubble/components/Bubble.tsx
@@ -45,7 +45,7 @@ export const Bubble = (props: BubbleProps) => {
           'z-index': 42424242,
         }}
         class={
-          `fixed sm:right-5 rounded-lg w-full sm:w-[800px] max-h-[704px]` +
+          `fixed sm:right-5 rounded-lg w-full sm:w-70vm max-h-[704px]` +
           (isBotOpened() ? ' opacity-1' : ' opacity-0 pointer-events-none') +
           (props.theme?.button?.size === 'large' ? ' bottom-24' : ' bottom-20')
         }
